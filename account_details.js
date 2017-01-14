879e4c4a84f1b5fb1c8f387672cadf6e497cfd07
KyraWayne <KyraWayne@Kyras-MacBook-Air.local>
2017-01-13 19:27:34 -0800

    WIP: color schemes

--- account_details.js
+++ account_details.js
@@ -6,8 +6,26 @@ function dispResDetails(str){
     if(el.display=="none"){
         el.display="block";
     }
-    else{
+    else if(el.display=="block"){
         el.display="none";
     }
+    else{
+        el.display="block";
+    }
     console.log(el.display);
+}
+
+function colorLabel(el){
+    el=String(str);
+    var el=document.getElementById("colorLabel").innerHTML;
+    if(str=="rdCirc") el="RED";
+    else if(str=="orngCirc")  el="ORANGE";
+    else if(str=="yllwCirc")  el="YELLOW";
+    else if(str=="grnCirc") el="GREEN";
+    else if(str=="blCirc") el="BLUE";
+    else if(str=="prplCirc") el="PURPLE";
+    else if(str=="grCirc") el="GRAY";
+    else if(str=="pnkCirc") el="PINK";
+    else if(str==null || str=="") el="";  
+            
 }
