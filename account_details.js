7e482d0638de83480be297b38901bf0e08512ffa
KyraWayne <KyraWayne@Kyras-MacBook-Air.local>
2017-01-25 16:53:14 -0800

    circlesWIP

--- account_details.js
+++ account_details.js
@@ -15,17 +15,37 @@ function dispResDetails(str){
     console.log(el.display);
 }
 
-function colorLabel(el){
-    el=String(str);
-    var el=document.getElementById("colorLabel").innerHTML;
-    if(str=="rdCirc") el="RED";
-    else if(str=="orngCirc")  el="ORANGE";
-    else if(str=="yllwCirc")  el="YELLOW";
-    else if(str=="grnCirc") el="GREEN";
-    else if(str=="blCirc") el="BLUE";
-    else if(str=="prplCirc") el="PURPLE";
-    else if(str=="grCirc") el="GRAY";
-    else if(str=="pnkCirc") el="PINK";
-    else if(str==null || str=="") el="";  
-            
+function colorLabel(str){
+    str=String(str);
+    var el=document.getElementById("colorLabel");
+    el.style.color="black";
+    if(str=="rdCirc"){
+        el.innerHTML="RED: energetic, passionate";
+    }
+    else if(str=="orngCirc"){
+        el.innerHTML="ORANGE: bold, cheerful";
+    }
+    else if(str=="yllwCirc"){
+        el.innerHTML="YELLOW: bright, social";
+    }
+    else if(str=="grnCirc"){
+        el.innerHTML="GREEN: sustainable, healthy";
+    }
+    else if(str=="blCirc"){
+        el.innerHTML="BLUE: calm, intelligent";
+    }
+    else if(str=="prplCirc"){ 
+        el.innerHTML="PURPLE: ambitious, wise";
+    }
+    else if(str=="gryCirc"){
+         el.innerHTML="GRAY: reliable, mature";
+    }
+    else if(str=="pnkCirc"){
+        el.innerHTML="PINK: compassionate, energetic";
+    }
+    else if(str=="clear"){
+        el.style.color="white";
+    }
 }
+
+
