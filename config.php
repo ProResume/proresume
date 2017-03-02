183f37b97378c2407968e69894fb63c3929be1f6
KyraWayne <KyraWayne@Kyras-MacBook-Air.local>
2017-03-01 17:34:40 -0800

    Php attempt

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< Updated upstream
  
 $config['callback_url']         =   'https://proresume.github.io/proresume/'; //Your callback URL
  
@@ -9,3 +10,27 @@ $config['Client_Secret']      =   'jxZbsXW1MPYHp87w';  // Your LinkedIn Applicat
 
 
 
+=======
+ini_set('display_errors','On');
+error_reporting(E_ALL);
+
+$dbh_host = "dbserver.engr.scu.edu";
+$db_user = "kyrawayne";
+$db_pw = "00000979812";
+$db_name = "sdb_kwayne";
+
+$conn = mysql_connect($dbh_host, $db_user, $db_pw, $db_name)
+		or die("Error" . mysqli_error($conn));
+
+$qry = "SHOW DATABASES";
+$result = $conn->query($qry);
+
+echo "Connected.  Dumping DB list:<br>\n";
+while($row = mysqli_fetch_assoc($result)) {
+        echo $row['Database'] . "<br>\n";
+}       
+
+mysqli_close($conn);
+
+?>
+>>>>>>> Stashed changes
--- proresume.sql
+++ proresume.sql
@@ -20,6 +20,13 @@ CREATE TABLE EducationInfo
 (
 	EduId int auto_increment NOT NULL,
     UserId int NOT NULL,
+    InstitutionName varchar(100),
+    YearStarted year,
+    YearEnded year,
+    Major varchar(100),
+    SecondMajor varchar(100),
+    Minor varchar(100),
+    SecondMinor varchar(100),
     PRIMARY KEY (EduId),
     FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
@@ -28,6 +35,11 @@ CREATE TABLE ExperienceInfo
 (
 	ExpId int auto_increment NOT NULL,
     UserId int NOT NULL,
+	CompanyName varchar(100),
+    YearStarted year,
+    YearEnded year,
+    JobRole varchar(100),
+    JobDescript varchar(250),
     PRIMARY KEY (ExpId),
     FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
@@ -36,6 +48,8 @@ CREATE TABLE SkillsInfo
 (
 	SkillId int auto_increment NOT NULL,
     UserId int NOT NULL,
+    Skill varchar(50),
+    Proficiency ENUM('1','2','3','4','5'),
     PRIMARY KEY (SkillId),
     FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
@@ -44,6 +58,11 @@ CREATE TABLE ProjectsInfo
 (
 	ProjId int auto_increment NOT NULL,
     UserId int NOT NULL,
+	OrganizationName varchar(100),
+    YearStarted year,
+    YearEnded year,
+    JobRole varchar(100),
+    JobDescript varchar(250),
     PRIMARY KEY (ProjId),
     FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
@@ -52,6 +71,7 @@ CREATE TABLE InterestsInfo
 (
 	InterestId int auto_increment NOT NULL,
     UserId int NOT NULL,
+    Interest varchar(50),
     PRIMARY KEY (InterestId),
     FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
@@ -60,6 +80,11 @@ CREATE TABLE VolunteerInfo
 (
 	VolunteerId int auto_increment NOT NULL,
     UserId int NOT NULL,
+	OrganizationName varchar(100),
+    YearStarted year,
+    YearEnded year,
+    JobRole varchar(100),
+    JobDescript varchar(250),
     PRIMARY KEY (VolunteerId),
     FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
@@ -69,10 +94,49 @@ CREATE TABLE AwardsInfo
 (
 	AwardId int auto_increment NOT NULL,
     UserId int NOT NULL,
+	AdministerName varchar(100),
+    YearReceived year,
+	AwardName varchar(100),
+    AwardDescript varchar(250),
     PRIMARY KEY (AwardId),
     FOREIGN KEY (UserId) REFERENCES Users(UserId)
 );
 
+CREATE TABLE ResumeTypes
+(
+	UserId int NOT NULL,
+	ResumeType ENUM('business','engineer','nonprofit','design','custom') NOT NULL,
+    ResumeId int NOT NULL,
+    PRIMARY KEY (ResumeType),
+    FOREIGN KEY (UserId) REFERENCES Users(UserId),
+    FOREIGN KEY (ResumeId) REFERENCES Resumes(ResumeId)
+);
 
+CREATE TABLE Resumes
+(
+	ResumeId int auto_increment NOT NULL,
+    UserId int NOT NULL,
+    ResumeType ENUM('business','engineer','nonprofit','design','custom') NOT NULL,
+    Header int,
+    Footer int,
+    Top int,
+    TopLeft int,
+    TopRight int,
+    Middle int,
+	MiddleLeft int,
+    MiddleRight int,
+    Bottom int,
+	BottomLeft int,
+    BottomRight int,
+    PRIMARY KEY (ResumeId),
+    FOREIGN KEY (ResumeType) REFERENCES ResumeTypes(ResumeType),
+    FOREIGN KEY (UserId) REFERENCES Users(UserId)
+);
+
+CREATE TABLE Items
+(
+	ItemId int auto_increment NOT NULL,
+    PRIMARY KEY (ItemId)
+);
 
 
